bf2daf90f0c8270f43861e5cf9388adfbbf11171
Stijn Opheide <stijn.opheide@vouch.io>
2020-07-29 15:22:55 +0200

    don't make assumptions on how the linuxkit image is built

--- scripts/update-image.sh
+++ scripts/update-image.sh
@@ -6,8 +6,6 @@ image=$1
 instance=$2
 volume=$(tm-machine.clj get-volume $instance "/dev/sda1")
 
-linuxkit build -format aws $1
 tm-machine.clj stop $instance $volume
-result_image=$(echo $image | sed 's/yml/raw/')
-sudo dd if=$result_image of=/dev/nvme1n1 bs=64k # todo: automatically detect /dev/nvme1 device name.
+sudo dd if=$image of=/dev/nvme1n1 bs=64k # todo: automatically detect /dev/nvme1 device name.
 tm-machine.clj start $instance $volume
