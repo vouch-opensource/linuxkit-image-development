a28631445500293db9fecc5c10b17d19981ab363
Raphael P. Ribeiro <raphael.ribeiro@vouch.io>
2021-03-02 08:29:46 -0300

    fix build, checkout and install linuxkit

--- dev_cycle/terraform/install.sh
+++ dev_cycle/terraform/install.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+# dependenciess
+
 apt-get update
 apt-get remove docker docker-engine docker.io containerd runc
 apt-get install -y golang-go git awscli unzip
@@ -10,6 +12,7 @@ apt-get install -y \
     ca-certificates \
     curl \
     gnupg-agent \
+    make \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
@@ -22,11 +25,20 @@ apt-get -y install docker-ce docker-ce-cli containerd.io qemu
 usermod -a -G docker ubuntu
 
 # install linuxkit
-wget ${linuxkit_download_url} -O /usr/local/bin/linuxkit
+
+export GOPATH=~/go
+mkdir -p $GOPATH
+go get github.com/linuxkit/linuxkit/src/cmd/linuxkit
+pushd $GOPATH/src/github.com/linuxkit/linuxkit
+git checkout ${linuxkit_version}
+make local-build
+popd
+cp $GOPATH/src/github.com/linuxkit/linuxkit/bin/linuxkit /usr/local/bin/
 chmod +x /usr/local/bin/linuxkit
 
 # install babashka
-curl -s ${babashka_download_url} -o install-babashka
+
+curl -s https://raw.githubusercontent.com/borkdude/babashka/${babashka_version}/install -o install-babashka
 chmod +x install-babashka
 ./install-babashka
 
